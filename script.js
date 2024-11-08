9ebbd85ec287da42c16849198e9c03a98766730b
BalavinayakamD <balavinayakamd.al24@bitsathy.ac.in>
2024-11-08 22:54:43 +0530

    I still have err

---
+++ script.js
@@ -0,0 +1,10 @@
+window.addEventListener('DOMContentLoaded',()=>{var enteredText = document.getElementById('enteredText');
+var totalChar = document.getElementById('totalChar');
+var remainingChar = document.getElementById('remainingChar');
+var countLimit = 100;
+function count(){
+    totalChar.textContent = enteredText.value.length;
+    remainingChar.textContent = countLimit - enteredText.value.length;
+}
+enteredText.addEventListener('input', count())
+})
